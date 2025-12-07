a8544ccd9f219fc15c968e6e8d36ff2e7fd666cb
Maruf Bepary <bepary71@gmail.com>
2025-12-07 13:23:35 +0000

    REFACTOR: Decomposed home recommendations card into separate files

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@
 import { PostVote } from "@/atoms/postsAtom";
 import CreatePostLink from "@/components/Community/CreatePostLink";
 import PersonalHome from "@/components/Community/PersonalHome";
-import Recommendations from "@/components/Community/Recommendations";
+import Recommendations from "@/components/Community/recommendations/Recommendations";
 import PageContent from "@/components/Layout/PageContent";
 import PostLoader from "@/components/Loaders/PostLoader";
 import PostItem from "@/components/Posts/PostItem";
